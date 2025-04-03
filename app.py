445061a964386f3dfe40587bcc34a380198dc29d
Esther Joseph <60444776+esther-joseph@users.noreply.github.com>
2025-04-02 20:59:35 -0500

    add api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -31,6 +31,27 @@ def delete_task(id):
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for('index'))
+
+# API Endpoints
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.order_by(Task.date).all()
+    return jsonify([{'id': task.id, 'title': task.title, 'date': task.date.strftime('%Y-%m-%d %H:%M')} for task in tasks])
+
+@app.route('/api/task', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], date=datetime.strptime(data['date'], '%Y-%m-%dT%H:%M'))
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created successfully'}), 201
+
+@app.route('/api/task/<int:id>', methods=['DELETE'])
+def api_delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted successfully'})
 
 if __name__ == '__main__':
     db.create_all()
